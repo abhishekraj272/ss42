f781f75d2707a9c5786e69934bdb4b213b5c7986
Abhishek Raj <abhishekraj272@gmail.com>
2020-06-22 21:04:09 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 import pyotp
 from django.core.mail import send_mail
+from company.models import Company
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 # Create your views here.
 
 def signuppage(request):
@@ -65,4 +68,43 @@ def confirm_verification(request, id, otp, complex):
 
 
     return HttpResponse(f"OTP wrong. Please try again :(")
+
+def company_generate_otp(request, id):
+    obj = Company.objects.get(id=id)
+
+    obj.otp = pyotp.random_base32()
+    token = pyotp.random_base32()
+    obj.save()
+
+    url = f'https://backend.scrapshut.com/api/verify/company/confirm/{token}/{id}/{obj.otp}'
+
+    try:
+        html_message = render_to_string(f'token.html', {'name': obj.dev_name, 'url': url})
+    except Exception as e:
+        return HttpResponse(e, status=404)
+
+    plain_message = strip_tags(html_message)
+        
+    from_email = f'Scrapshut pythonautomail1@gmail.com'
+    subject = 'Verify Your Email with Scrapshut'
+    try:
+        send_mail(subject, plain_message, from_email, [obj.email], html_message=html_message)
+    except Exception as e:
+        HttpResponse(e, status=403)
+    
+    return HttpResponse(f'Verification link sent to your email :)',status=200)
+
+
+def company_otp_verify(request, id, otp, complex):
+    obj = Company.objects.get(id=id)
+
+    obj.is_verified = True
+    obj.save()
+
+    if obj.otp == otp:
+        return HttpResponse(f"Email successfully verified for {obj.dev_name} :)", status=200)    
+
+
+    return HttpResponse(f"OTP wrong. Please try again :(")
+
     
