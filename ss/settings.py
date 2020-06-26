fc32ddeb64146fbd4c53d968487482aff4055bf9
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-06-26 19:43:46 +0530

    working

--- ss/settings.py
+++ ss/settings.py
@@ -31,6 +31,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'https://wiringbridge.com',
 ]
+CORS_ORIGIN_ALLOW_ALL=True
 
 CORS_ALLOW_HEADERS = (
         'x-requested-with',
@@ -40,7 +41,7 @@ CORS_ALLOW_HEADERS = (
         'authorization',
         'x-csrftoken',
         'api-key',
-        'API-KEY'
+        'API-KEY',
 )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
