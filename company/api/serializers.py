8789b78a29bbc4f1e2362b086825c33d13eedcca
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-06-26 19:17:06 +0530

    it's perfect upto here

--- company/api/serializers.py
+++ company/api/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from company.models import Company
 
 class CompanySerializer(serializers.ModelSerializer):
-
+    # password2=serializers.CharField(style={'input_type':'password'},write_only=True)
     class Meta:
         model = Company
         # fields = ('dev_name', 'email', 'city', 'country', 'description')
         fields = '__all__'
-
+        # extra_kwargs={
+        #     'password':{'write_only'=True}
+        # }
 
 class CompanySerializer_read(serializers.ModelSerializer):
 
