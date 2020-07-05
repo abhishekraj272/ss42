25fe906a0ecc7e6fbe951effee2baaa8542e8c2b
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-07-05 23:06:55 +0530

    added search endpoint

--- ss/settings.py
+++ ss/settings.py
@@ -150,19 +150,19 @@ WSGI_APPLICATION = 'ss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#
-# DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#            'NAME': 'scrapshut',
-#            'USER': 'postgres',
-#            'PASSWORD': 'pass',
-#            'HOST': 'localhost',
-#            'PORT': '',
+
+DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'scrapshut',
+           'USER': 'postgres',
+           'PASSWORD': 'pass',
+           'HOST': 'localhost',
+           'PORT': '',
 
 
-#        }
-#    }
+       }
+   }
 # # DATA_UPLOAD_MAX_NUMBER_FIELDS = 100240 # higher than the count of fields
 DATA_UPLOAD_MAX_MEMORY_SIZE = 100000000
 
@@ -173,16 +173,16 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 100000000
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'ss',
-       'USER': 'postgres',
-       'PASSWORD': 'qwerty31',
-       'HOST': 'localhost',
-        'PORT': '',
-    }
- }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'ss',
+#        'USER': 'postgres',
+#        'PASSWORD': 'qwerty31',
+#        'HOST': 'localhost',
+#         'PORT': '',
+#     }
+#  }
 #
 # DATABASES = {
 #   'default': {
